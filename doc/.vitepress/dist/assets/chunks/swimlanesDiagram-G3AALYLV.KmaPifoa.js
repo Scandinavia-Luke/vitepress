34213c632040aedd81cad8a42f6a068a7e2cc75b
Scandinavia-Luke <lx-luke@outlook.com>
2026-07-05 10:15:34 +0800

    修改页脚menu后重新部署

--- doc/.vitepress/dist/assets/chunks/swimlanesDiagram-G3AALYLV.KmaPifoa.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U.ttojDUbO.js";import{_ as a}from"../app.BV-mDsyS.js";import"./chunk-5VM5RSS4.gLVAgAVg.js";import"./chunk-XXDRQBXY.DYrUK0RZ.js";import"./chunk-VR4S4FIN.D5ikaO9i.js";import"./chunk-32BRIVSS.Dg3hnLW7.js";import"./channel.BUiiohy_.js";import"./framework.1ZaJrroY.js";import"./theme.DWyhpGs1.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
